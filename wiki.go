bdb54cad2bc9565622b1b38cd89b9afc8ea70bcf
syo-sa1982 <fujii1308@gmail.com>
2015-08-05 09:33:42 +0900

    add render method

--- wiki.go
+++ wiki.go
@@ -25,6 +25,11 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
