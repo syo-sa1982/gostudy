fcd497a0a8f39d45a9849545e51abb82dae6df97
syo-sa1982 <fujii1308@gmail.com>
2015-08-13 09:22:00 +0900

    Introducing Function Literals and Closures

--- wiki.go
+++ wiki.go
@@ -3,9 +3,13 @@ package main
 import (
 	"html/template"
 	"io/ioutil"
+//	"net"
 	"net/http"
+
 	"regexp"
 	"errors"
+	"flag"
+//	"log"
 )
 
 type Page struct {
@@ -45,28 +49,61 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+//
+//func viewHandler(w http.ResponseWriter, r *http.Request) {
+//	title, err := getTitle(w, r)
+//	if err != nil {
+//		return
+//	}
+//	p, err := loadPage(title)
+//	if err != nil {
+//		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+//		return
+//	}
+//	renderTemplate(w, "view", p)
+//}
+//
+//func editHandler(w http.ResponseWriter, r *http.Request) {
+//	title, err := getTitle(w, r)
+//	if err != nil {
+//		return
+//	}
+//	p, err := loadPage(title)
+//	if err != nil {
+//		p = &Page{Title: title}
+//	}
+//	renderTemplate(w, "edit", p)
+//}
+//func saveHandler(w http.ResponseWriter, r *http.Request) {
+//	title, err := getTitle(w, r)
+//	if err != nil {
+//		return
+//	}
+//	body := r.FormValue("body")
+//	p := &Page{Title: title, Body: []byte(body)}
+//	err = p.save()
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		return
+//	}
+//	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+//}
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-
 	renderTemplate(w, "view", p)
 }
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
 }
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -77,10 +114,37 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	flag.Parse()
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+//
+//		l, err := net.Listen("tcp", "127.0.0.1:0")
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		err = ioutil.WriteFile("final-port.txt", []byte(l.Addr().String()), 0644)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		s := &http.Server{}
+//		s.Serve(l)
+//		return
+//	if *addr {
+//
+//	}
 	http.ListenAndServe(":8080", nil)
 }
