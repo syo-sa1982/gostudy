0909c3a5f1f00d630775e78703f7244c58e1970c
syo-sa1982 <fujii1308@gmail.com>
2015-12-02 08:32:36 +0900

    WaitGroup

---
+++ multi_practice/waitGroup.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"sync"
+	"log"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i:=0; i<3; i++ {
+		wg.Add(1)
+		go func(i int) {
+			log.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
