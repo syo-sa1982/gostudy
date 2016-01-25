6ce09d9bb92a56a2935a64b21582695bdb9be9c6
syo-sa1982 <fujii1308@gmail.com>
2016-01-26 08:23:13 +0900

    テンプレートを分離

--- chat/main.go
+++ chat/main.go
@@ -1,22 +1,28 @@
 package main
+
 import (
-	"net/http"
+	"html/template"
 	"log"
+	"net/http"
+	"sync"
+	"path/filepath"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+// HTTPリクエストを処理
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Webサーバを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
