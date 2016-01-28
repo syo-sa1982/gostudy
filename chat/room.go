4fd0cf27a8ac468e7be64738b0ff3e5be86564e7
syo-sa1982 <fujii1308@gmail.com>
2016-01-29 07:55:45 +0900

    チャットルームのモデル化

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,35 @@
 package main
 
 type room struct {
+	// 転送メッセージ
 	forward chan []byte
+	// 入室チャネル
+	join chan *client
+	// 退室チャネル
+	leave chan *client
+	// 在室中
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
