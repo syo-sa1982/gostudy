fed72bcd20f6ba05c7e0b95a21ab2e75ae875f08
syo-sa1982 <fujii1308@gmail.com>
2016-02-01 09:04:53 +0900

    チャットルーム生成

--- chat/main.go
+++ chat/main.go
@@ -22,7 +22,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャット開始
+	go r.run()
 	// Webサーバを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
