5d4e087a05b3228e5a4d56549f09b79ac745c901
syo-sa1982 <fujii1308@gmail.com>
2016-02-04 07:59:54 +0900

    動く状態にした

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"sync"
 	"path/filepath"
+	"flag"
 )
 
 type templateHandler struct {
@@ -18,16 +19,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャット開始
 	go r.run()
 	// Webサーバを開始
+	log.Println("Webサーバー開始。ポート:", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
--- chat/room.go
+++ chat/room.go
@@ -2,7 +2,6 @@ package main
 import (
 	"github.com/gorilla/websocket"
 	"net/http"
-	"github.com/revel/revel"
 	"log"
 )
 
