f76c5f8700b8abd9a708de68e2d2243ce24b251c
syo-sa1982 <fujii1308@gmail.com>
2016-02-08 07:42:21 +0900

    テストコードに沿って実装

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,10 +1,22 @@
 package trace
-import "io"
+import (
+	"io"
+	"fmt"
+)
 
 type Tracer interface {
 	Trace(...interface{})
 }
 
 func New(w io.Writer) Tracer {
-	return nil
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer)Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
