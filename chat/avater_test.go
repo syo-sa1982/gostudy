021fe58f40d29baa12bb7fcc5a22a41720c5f133
syo-sa1982 <fujii1308@gmail.com>
2016-02-25 07:57:10 +0900

    Avatarのテスト書いた

---
+++ chat/avater_test.go
@@ -0,0 +1,25 @@
+package main
+import "testing"
+
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("値がないなら、AuthAvatar.GetAvatarURLはErrNoAvatarURLを返すべき")
+	}
+	// 値をSET
+	testUrl := "http://url-to-avatar/"
+	client.userData = map[string]interface{}{"avatar_url": testUrl}
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("値があるなら、AuthAvatar.GetAvatarURLはエラーを返すべきでない")
+	} else {
+		if url != testUrl {
+			t.Error("AuthAvatar.GetAvatarURLは正しいURLを返すべき")
+		}
+	}
+}
+
+
