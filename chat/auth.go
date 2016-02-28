afd0d49af75767967f6da912bf77eba4b6a86d7f
syo-sa1982 <fujii1308@gmail.com>
2016-02-29 08:15:44 +0900

    ユーザーID追加

--- chat/auth.go
+++ chat/auth.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/objx"
+	"crypto/md5"
+	"io"
 )
 
 type authHandler struct {
@@ -56,7 +58,11 @@ func loginHandler(w http.ResponseWriter, r *http.Request)  {
 		if err != nil {
 			log.Fatalln("認証を完了できませんでした:", provider, "", err)
 		}
+		m := md5.New()
+		io.WriteString(m, strings.ToLower(user.Name()))
+		userID := fmt.Sprintf("%x", m.Sum(nil))
 		authCookieValue := objx.New(map[string]interface{}{
+			"userid": userID,
 			"name": user.Name(),
 			"avater_url": user.AvatarURL(),
 			"email": user.Email(),
