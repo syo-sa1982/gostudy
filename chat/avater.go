a41adee252a31b5f5a9d8bad8fb63bfaedd72632
syo-sa1982 <fujii1308@gmail.com>
2016-03-03 18:31:17 +0900

    チェック内容を変更した

--- chat/avater.go
+++ chat/avater.go
@@ -28,11 +28,9 @@ func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
 type GravatarAvatar struct {}
 var UseGravatar GravatarAvatar
 func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if email, ok := c.userData["email"]; ok {
-		if emailStr, ok := email.(string); ok {
-			m := md5.New()
-			io.WriteString(m, strings.ToLower(emailStr))
-			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
+	if userid, ok := c.userData["userid"]; ok {
+		if useridStr, ok := userid.(string); ok {
+			return "//www.gravatar.com/avatar/" + useridStr, nil
 		}
 	}
 	return "", ErrNoAvatarURL
