a97b7d7a0296abee9305a1769b114ba6989122ad
syo-sa1982 <fujii1308@gmail.com>
2016-03-14 09:09:26 +0900

    ハンドラつけ忘れた

--- chat/main.go
+++ chat/main.go
@@ -50,7 +50,10 @@ func main() {
 
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir("./avatars"))))
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/uploader", uploaderHandler)
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
 			Name: "auth",
