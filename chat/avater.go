0f44a8cbda6e9bb29ea20267dfa4bb90ddb6c04d
syo-sa1982 <fujii1308@gmail.com>
2016-03-23 08:07:16 +0900

    ビルドエラー回避

--- chat/avater.go
+++ chat/avater.go
@@ -9,32 +9,32 @@ var ErrNoAvatarURL = errors.New("chat: アバターのURLないぞ？")
 type Avatar interface {
 	GetAvatarURL(c *client) (string, error)
 }
-
-type AuthAvatar struct {}
-var UseAuthAvatar AuthAvatar
-func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlStr, ok := url.(string); ok {
-			return urlStr, nil
-		}
-	}
-	return "", ErrNoAvatarURL
-}
-
-type GravatarAvatar struct {}
-var UseGravatar GravatarAvatar
-func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok {
-			return "//www.gravatar.com/avatar/" + useridStr, nil
-		}
-	}
-	return "", ErrNoAvatarURL
-}
+//
+//type AuthAvatar struct {}
+//var UseAuthAvatar AuthAvatar
+//func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+//	if url, ok := c.userData["avatar_url"]; ok {
+//		if urlStr, ok := url.(string); ok {
+//			return urlStr, nil
+//		}
+//	}
+//	return "", ErrNoAvatarURL
+//}
+//
+//type GravatarAvatar struct {}
+//var UseGravatar GravatarAvatar
+//func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+//	if userid, ok := c.userData["userid"]; ok {
+//		if useridStr, ok := userid.(string); ok {
+//			return "//www.gravatar.com/avatar/" + useridStr, nil
+//		}
+//	}
+//	return "", ErrNoAvatarURL
+//}
 
 type FileSystemAvatar struct {}
 var UseFileSystemAvatar FileSystemAvatar
-func (_ FileSystemAvatar) GetAvaterURL(c *client) (string, error) {
+func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
 	if userid, ok := c.userData["userid"]; ok {
 		if useridStr, ok := userid.(string); ok {
 			return "/avaters/" + useridStr + ".jpg", nil
